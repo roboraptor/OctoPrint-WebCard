e8a86b4000c68eee94d56ac71b144b1e8b096aaf
Vojtěch T <eroboraptor@gmail.com>
2026-03-20 16:42:55 +0100

    test

--- octoprint_webcard/static/js/webcard.js
+++ octoprint_webcard/static/js/webcard.js
@@ -4,15 +4,11 @@ $(function() {
         // The settingsViewModel input parameter is injected by OctoPrint
         self.settings = parameters[0];
 
-        console.log("--- WebCard --- WebCardViewModel created");
-        console.log("--- WebCard --- Initial URL:", self.settings.settings.plugins.webcard.webpage_url());
-
         // Observable for the current URL, loaded from the plugin settings
         self.currentUrl = ko.observable(self.settings.plugins.webcard.webpage_url());
 
         // Watch for changes to the URL in the settings and update the observable
         self.settings.plugins.webcard.webpage_url.subscribe(function(newVal) {
-            console.log("--- WebCard --- URL changed to:", newVal);
             self.currentUrl(newVal);
         });
     }
