7e2a830e05845c78cc250828e20628c4d2e198f9
Vojtěch T <eroboraptor@gmail.com>
2026-03-20 17:00:33 +0100

    test

--- octoprint_webcard/static/js/webcard.js
+++ octoprint_webcard/static/js/webcard.js
@@ -5,10 +5,10 @@ $(function() {
         self.settings = parameters[0];
 
         // Observable for the current URL, loaded from the plugin settings
-        self.currentUrl = ko.observable(self.settings.plugins.webcard.webpage_url());
+        self.currentUrl = ko.observable(self.settings.settings.plugins.webcard.webpage_url());
 
         // Watch for changes to the URL in the settings and update the observable
-        self.settings.plugins.webcard.webpage_url.subscribe(function(newVal) {
+        self.settings.settings.plugins.webcard.webpage_url.subscribe(function(newVal) {
             self.currentUrl(newVal);
         });
     }
